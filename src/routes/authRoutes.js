d06e88108a0a9716b652843ee1b1d0f7daadf232
yogeshkr2002 <yogeshkumarsharma689@gmail.com>
2025-01-15 17:23:32 +0530

    Login and Logout Successful

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,8 +3,13 @@ import { login, logout, signup } from "../controllers/authController.js";
 
 const router = express.Router();
 
+// Register/Sign Up
 router.post("/signup", signup);
+
+// Login
 router.post("/login", login);
+
+// Logout
 router.post("/logout", logout);
 
 export default router;
