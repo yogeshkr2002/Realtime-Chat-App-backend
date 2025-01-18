88c09c127a7efcd21af38f93083e70ce14c930b4
yogeshkr2002 <yogeshkumarsharma689@gmail.com>
2025-01-18 11:37:11 +0530

    cors issue has been resolved

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 dotenv.config();
 
 const PORT = process.env.PORT;
