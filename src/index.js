97d80ce61016a431180935841f669a91eb3a348f
yogeshkr2002 <yogeshkumarsharma689@gmail.com>
2025-02-13 11:37:57 +0530

    Changes made in index file to add socket.io

--- src/index.js
+++ src/index.js
@@ -5,23 +5,38 @@ import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import path from "path";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
+// const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => {
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
+
+server.listen(PORT, () => {
   console.log(`Server is running on PORT : ${PORT}`);
   connectDB();
 });
